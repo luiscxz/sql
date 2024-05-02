dcc42282ecc6c6391650891d0d6039ff9b7ada1a
Luiscxz <lionsgateazul@gmail.com>
2024-05-02 15:02:44 -0500

    Se modifica el algoritmo y se deja solo la creación de la tabla modulo_victimicación y se agregan registos y se realizan consultas

--- create_database_create_table.sql
+++ create_database_create_table.sql
@@ -1,7 +1,25 @@
--- creando tabla
-create table mi_primer_tabla(columna1 varchar(20),columna2 int);
--- insertando  registros a la base de datos
-insert into mi_primer_tabla values ('Hector Manuel',3),('Roberto',20);
-insert into mi_primer_tabla values ('Luis',5);
---  procedemos a ver los registros de la tabla
-select *from mi_primer_tabla;
+-- creando tabla modulo victimizacion
+CREATE TABLE modulo_victimizacion(
+	entidad varchar(2),
+	viv varchar(5),
+	hogar varchar(5),     
+	upm varchar(5),     
+	renglon varchar(20), 	
+	tipo_delito int,     
+	num_delito int,     
+	nombre varchar(50), 	
+	apellido varchar(50), 	
+	perdida int);
+	--insertando registros a la tabla
+	INSERT INTO modulo_victimizacion VALUES ('01','15','03','15','10',3, 1, 'Luis', 'Miranda',2000),        
+								 		('01','15','03','15','10',3, 2, 'Luis', 'Miranda',3000),        
+										('02','20','13','10','03',4, 1, 'Oscar', 'Ruiz',1000),        
+										('09','16','11','05','01',5, 1, 'Raul', 'Ortiz',4500),        
+										('32','12','11','02','04',2, 1, 'Luisa', 'Ortiz',300),        
+										('32','12','11','02','04',3, 1, 'Luisa', 'Ortiz',500);
+-- procedemos a visualizar las columnas de interes "tipodelito y pérdida"
+select tipo_delito,perdida from modulo_victimizacion;
+-- Seleccionando toda la tabla y ordenando por la columna apellido de forma ascendente
+select *from modulo_victimizacion order by apellido asc;  
+-- ordenando por apellido asc y perdida descendente, y que solo me muesta las columnas nombre, apellido, pérdida
+select nombre,apellido,perdida from modulo_victimizacion order by apellido asc,perdida desc;
